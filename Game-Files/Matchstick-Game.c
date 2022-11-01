f4573cfa6bf2f54c4b19bc3e3584a6b204423ef2
Alkaison <505ganeshmourya@gmail.com>
2022-11-01 14:48:31 +0530

    added welcome screen message and game rules

--- Game-Files/Matchstick-Game.c
+++ Game-Files/Matchstick-Game.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
 
 // Firstly only making player vs computer then player vs player
 void welcomescreen(void);
@@ -7,18 +9,34 @@ void player(void);
 void computer(void);
 void scoreboard(void);
 void highscore(void);   
+void cleaner(void);
 
 int main(){
-    printf("Started At 01/11/2022 13:30 +05:30 GMT By Ganesh Mourya (Alkaiosn)!");
+
+    welcomescreen();  
+    menu();
     return 0;
 }
 
 void welcomescreen(){
 
+    cleaner();
+    printf("-----------------------------------------\n");
+    printf(">>> Welcome to 21 Matchstick Game <<< \n");
+    printf("-----------------------------------------\n\n");
+    printf(">> Game Rules: \n\n");
+    printf("1. There are 21 matchsticks. \n");
+    printf("2. The computer asks the player to pick 1, 2, 3 or 4 matchsticks. \n");
+    printf("3. After the person picks, the computer does its picking. \n");
+    printf("4. Whoever is forced to pick up the last matchstick loses the game. \n\n");
+
+    printf("Press any key to start... \n");
+    getch();
 }
 
 void menu(){
-
+    cleaner();
+    printf("Welcome to Menu");
 }
 
 void player(){
@@ -35,4 +53,9 @@ void scoreboard(){
 
 void highscore(){
     
+}
+
+void cleaner(){
+    system("cls");
+    fflush(stdin);
 }
