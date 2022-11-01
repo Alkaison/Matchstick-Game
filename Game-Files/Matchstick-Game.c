4c5be3ab769792cdd69ec8555f03d94ca60f2ba1
Alkaison <505ganeshmourya@gmail.com>
2022-11-01 13:55:11 +0530

    added all intial functions

--- Game-Files/Matchstick-Game.c
+++ Game-Files/Matchstick-Game.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
 
+// Firstly only making player vs computer then player vs player
+void welcomescreen(void);
+void menu(void);
+void player(void);
+void computer(void);
+void scoreboard(void);
+void highscore(void);   
+
 int main(){
     printf("Started At 01/11/2022 13:30 +05:30 GMT By Ganesh Mourya (Alkaiosn)!");
     return 0;
+}
+
+void welcomescreen(){
+
+}
+
+void menu(){
+
+}
+
+void player(){
+
+}
+
+void computer(){
+
+}
+
+void scoreboard(){
+
+}
+
+void highscore(){
+    
 }
