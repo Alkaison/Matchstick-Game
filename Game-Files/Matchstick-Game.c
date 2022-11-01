568600617ea6b7f42a5ab4baf376c9f198c8489f
Alkaison <505ganeshmourya@gmail.com>
2022-11-01 17:33:20 +0530

    added player vs ai gamemode

--- Game-Files/Matchstick-Game.c
+++ Game-Files/Matchstick-Game.c
@@ -30,9 +30,9 @@ void cleaner(){
 void welcomescreen(){
 
     cleaner();
-    printf("-----------------------------------------\n");
+    printf("--------------------------------------\n");
     printf(">>> Welcome to 21 Matchstick Game <<< \n");
-    printf("-----------------------------------------\n\n");
+    printf("--------------------------------------\n\n");
     printf(">> Rules: \n");
     printf("1. There are 21 matchsticks. \n");
     printf("2. The computer asks the player to pick 1, 2, 3 or 4 matchsticks. \n");
@@ -50,9 +50,9 @@ void menu(){
 
 label1:
     cleaner();
-    printf("-----------------------------------------\n");
+    printf("---------------------------\n");
     printf(">>> Select your choice <<< \n");
-    printf("-----------------------------------------\n\n");
+    printf("---------------------------\n\n");
     printf("> 1. Player vs Computer \n");
     printf("> 2. Player vs Player \n");
     printf("> 3. Player vs A.I. (Impossible)\n");
@@ -83,8 +83,12 @@ label1:
 }
 
 void playground1(){
-
+    
     cleaner();
+    printf("----------------------------\n");
+    printf(">>> Player V/s Computer <<< \n");
+    printf("----------------------------\n\n");
+
 label2:
     printf("\nNumber of Match Sticks left: %d \n", sticks);
     printf("Pick between 1 to 4 sticks: ");
@@ -101,10 +105,10 @@ label2:
         if(sticks == 1)
         {
             sticks = 21;
-            printf("\nComputer Picked the Last Stick \n");
-            printf("-------------------------\n");
+            printf("\nComputer Picked the Last Stick \n\n");
+            printf("-------------------\n");
             printf(">>> Player Won <<< \n");
-            printf("-------------------------\n\n");
+            printf("-------------------\n\n");
             printf("Press any key to continue... \n");
             getch();
             menu();
@@ -129,10 +133,10 @@ label3:
         else if(sticks == 1)
         {
             sticks = 21;
-            printf("\nYou Picked the Last Stick! \n");
-            printf("-------------------------\n");
+            printf("\nYou Picked the Last Stick! \n\n");
+            printf("---------------------\n");
             printf(">>> Computer Won <<< \n");
-            printf("-------------------------\n\n");
+            printf("---------------------\n\n");
             printf("Press any key to continue... \n");
             getch();
             menu();
@@ -145,15 +149,75 @@ void playground2(){
 }
 
 void playground3(){
-    printf("PlayGround 3");
+
+    cleaner();
+    printf("------------------------\n");
+    printf(">>> Player V/s A.I. <<< \n");
+    printf("------------------------\n\n");
+
+label2:
+    printf("\nNumber of Match Sticks left: %d \n", sticks);
+    printf("Pick between 1 to 4 sticks: ");
+    scanf("%d",&player1);
+    fflush(stdin);
+    if(player1 > 4 || player1 < 1 || player1 >= sticks)
+    {
+        printf("\nError (%d): Invalid Input. \n", player1);
+        goto label2;
+    }
+    else
+    {
+        sticks -= player1;
+        if(sticks == 1)
+        {
+            sticks = 21;
+            printf("\nA.I. Picked the Last Stick \n\n");
+            printf("-------------------\n");
+            printf(">>> Player Won <<< \n");
+            printf("-------------------\n\n");
+            printf("Press any key to continue... \n");
+            getch();
+            menu();
+        }
+label3:
+        /* srand(time(0));
+        computer = (rand() % MAX) + MIN;
+
+        if (computer >= sticks)
+        {
+            goto label3;
+        } */
+
+        computer = 5 - player1;
+        
+        printf("\nNumber of Match Sticks left: %d \n", sticks);
+        printf("A.I. picked: %d \n", computer);
+        sticks -= computer;
+
+        if(sticks > 1)
+        {
+            goto label2;
+        }
+        else if(sticks == 1)
+        {
+            sticks = 21;
+            printf("\nYou Picked the Last Stick! \n\n");
+            printf("---------------------\n");
+            printf(">>> A.I. Won <<< \n");
+            printf("---------------------\n\n");
+            printf("Press any key to continue... \n");
+            getch();
+            menu();
+        }
+    }
 }
 
 void endscreen(){
     
     cleaner();
-    printf("----------------------------------------------\n");
-    printf(">>> Creator: @Alkaison (Ganesh Mourya) <<< \n");
-    printf("----------------------------------------------\n\n");
+    printf("-------------------------------------------\n");
+    printf(" >>> Creator: @Alkaison (Ganesh Mourya) <<< \n");
+    printf("-------------------------------------------\n\n");
 
     printf("> Github: https://github.com/Alkaison \n");
     printf("> Twitter: https://twitter.com/Alkaison \n");
