82e4e4cbab342a0570d5494537d8793daa406c8b
Alkaison <505ganeshmourya@gmail.com>
2022-11-01 17:15:47 +0530

    added player vs player gamemode

--- Game-Files/Matchstick-Game.c
+++ Game-Files/Matchstick-Game.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
+#include<time.h>
 
-// Firstly only making player vs computer then player vs player
+int sticks=21, player1=0, computer=0, player2=0;
+    const int MIN = 1;
+    const int MAX = 4;
+ 
 void welcomescreen(void);
 void menu(void);
-void player(void);
-void computer(void);
 void playground1(void);
 void playground2(void);
+void playground3(void);
 void endscreen(void);
 void cleaner(void);
-
-int sticks, player1, machine, player2;
 
 int main(){
 
@@ -32,11 +33,12 @@ void welcomescreen(){
     printf("-----------------------------------------\n");
     printf(">>> Welcome to 21 Matchstick Game <<< \n");
     printf("-----------------------------------------\n\n");
-    printf(">> Game Rules: \n\n");
+    printf(">> Rules: \n");
     printf("1. There are 21 matchsticks. \n");
     printf("2. The computer asks the player to pick 1, 2, 3 or 4 matchsticks. \n");
     printf("3. After the person picks, the computer does its picking. \n");
-    printf("4. Whoever is forced to pick up the last matchstick loses the game. \n\n");
+    printf("4. Pick less than the available sticks to win. \n");
+    printf("5. Whoever is forced to pick up the last matchstick loses the game. \n\n");
 
     printf("Press any key to start... \n");
     getch();
@@ -46,14 +48,15 @@ void menu(){
 
     int input=0;
 
-lable1:
+label1:
     cleaner();
     printf("-----------------------------------------\n");
     printf(">>> Select your choice <<< \n");
     printf("-----------------------------------------\n\n");
     printf("> 1. Player vs Computer \n");
     printf("> 2. Player vs Player \n");
-    printf("> 3. Exit Game \n");
+    printf("> 3. Player vs A.I. (Impossible)\n");
+    printf("> 4. Exit Game \n\n");
 
     printf("> Enter the number & hit ENTER: ");
     scanf("%d",&input);
@@ -67,29 +70,82 @@ lable1:
             playground2();
             break;
         case 3:
+            playground3();
+            break;
+        case 4:
             endscreen();
             break;
         default:
-            printf("Invalid Input, Press any key to continue... \n");
+            printf("Invalid Input, Press any key to retry... ");
             getch();
-            goto lable1;
+            goto label1;
     }
 }
 
-void player(){
-    printf("Player");
-}
-
-void computer(){
-    printf("Computer");
-}
-
 void playground1(){
-    printf("PlayGround 1");
+
+    cleaner();
+label2:
+    printf("\nNumber of Match Sticks left: %d \n", sticks);
+    printf("Pick between 1 to 4 sticks: ");
+    scanf("%d",&player1);
+    fflush(stdin);
+    if(player1 > 4 || player1 < 1 || player1 >= sticks)
+    {
+        printf("\nError (%d): Invalid Input. \n", player1);
+        goto label2;
+    }
+    else
+    {
+        sticks -= player1;
+        if(sticks == 1)
+        {
+            sticks = 21;
+            printf("\nComputer Picked the Last Stick \n");
+            printf("-------------------------\n");
+            printf(">>> Player Won <<< \n");
+            printf("-------------------------\n\n");
+            printf("Press any key to continue... \n");
+            getch();
+            menu();
+        }
+label3:
+        srand(time(0));
+        computer = (rand() % MAX) + MIN;
+
+        if (computer >= sticks)
+        {
+            goto label3;
+        }
+        
+        printf("\nNumber of Match Sticks left: %d \n", sticks);
+        printf("Computer picked: %d \n", computer);
+        sticks -= computer;
+
+        if(sticks > 1)
+        {
+            goto label2;
+        }
+        else if(sticks == 1)
+        {
+            sticks = 21;
+            printf("\nYou Picked the Last Stick! \n");
+            printf("-------------------------\n");
+            printf(">>> Computer Won <<< \n");
+            printf("-------------------------\n\n");
+            printf("Press any key to continue... \n");
+            getch();
+            menu();
+        }
+    }
 }
 
 void playground2(){
     printf("PlayGround 2");
+}
+
+void playground3(){
+    printf("PlayGround 3");
 }
 
 void endscreen(){
